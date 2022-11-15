331074929c45cfbe27c57bd97a2c5c53dd197737
odaialazzeh <odai.alazzeh@gmail.com>
2022-11-15 17:39:45 +0200

    Update JS file

--- function.js
+++ function.js
@@ -22,58 +22,48 @@ contactBtn.addEventListener('click', display);
 
 const popup = {
   name: 'popup',
-  img: ['images/SnapshootPortfolio.svg', 'images/Icon-Cancel-pop.svg', 'images/Disable-pop.svg', 'images/SnapshootPortfolio1.png'],
+  img: ['images/Web-d.png', 'images/Icon-Cancel-pop.svg', 'images/Disable-pop.svg', 'images/Web-d.png'],
   imgId: ['imgs', 'imgExit', 'imgx', 'imgsd'],
-  heading: 'Keeping track of hundreds of components',
-  technologies: ['Ruby on rails', 'css', 'JavScript'],
-  technologies1: ['Codekit', 'GitHub', 'Javasript', 'Bootstrap', 'Terminal', 'Codepen'],
-  paragraph: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
-  liveversion: ['#', 'See Live', 'images/Icon-Export.svg'],
-  source: ['#', 'See Source', 'images/Icon-GitHub.svg'],
+  heading: 'Web Development Course',
+  technologies: ['HTML', 'CSS', 'JavScript'],
+  technologies1: ['GitHub', 'Javasript', 'Bootstrap'],
+  paragraph: "This course offers all the details that you require to fully learn about the world of web programming and coding. You will learn all about web programming from scratch and willbe able to design your own websites by the end of this course.",
+  liveversion: ['https://odaialazzeh.github.io/capstone1-project/home.html', 'See Live', 'images/Icon-Export.svg'],
+  source: ['https://github.com/odaialazzeh/capstone1-project', 'See Source', 'images/Icon-GitHub.svg'],
 };
 
 const popup1 = {
   name: 'popup1',
-  img: ['images/SnapshootPortfolioP1.png', 'images/Icon-Cancel-pop.svg', 'images/Disable-pop.svg', 'images/SnapshootPortfolioP1d.png'],
+  img: ['images/list.png', 'images/Icon-Cancel-pop.svg', 'images/Disable-pop.svg', 'images/list.png'],
   imgId: ['imgs', 'imgExit1', 'imgx1', 'imgsd'],
-  heading: 'Tonic',
+  heading: 'To Do List',
   technologies: ['Ruby on rails', 'css', 'JavScript'],
   technologies1: ['Codekit', 'GitHub', 'Javasript', 'Bootstrap', 'Terminal', 'Codepen'],
-  paragraph: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
-  liveversion: ['#', 'See Live', 'images/Icon-Export.svg'],
-  source: ['#', 'See Source', 'images/Icon-GitHub.svg'],
+  paragraph: "In this project, you i will build a simple HTML list of To Do tasks.To-do list is a tool that helps to organize your day. It simply lists the things that you need to do and allows you to mark them as complete. You will build a simple website that allows for doing that, and you will do it using ES6 and Webpack!",
+  liveversion: ['https://odaialazzeh.github.io/To-Do-list/dist/index.html', 'See Live', 'images/Icon-Export.svg'],
+  source: ['https://github.com/odaialazzeh/To-Do-list', 'See Source', 'images/Icon-GitHub.svg'],
 };
 
 const popup2 = {
   name: 'popup2',
-  img: ['images/SnapshootPortfolio2.png', 'images/Icon-Cancel-pop.svg', 'images/Disable-pop.svg', 'images/SnapshootPortfolioD2.png'],
+  img: ['images/books.png', 'images/Icon-Cancel-pop.svg', 'images/Disable-pop.svg', 'images/books.png'],
   imgId: ['imgs', 'imgExit2', 'imgx2', 'imgsd'],
-  heading: 'Profesional Art Printing Data',
+  heading: 'Awesome books',
   technologies: ['Ruby on rails', 'css', 'JavScript'],
   technologies1: ['Codekit', 'GitHub', 'Javasript', 'Bootstrap', 'Terminal', 'Codepen'],
-  paragraph: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
-  liveversion: ['#', 'See Live', 'images/Icon-Export.svg'],
-  source: ['#', 'See Source', 'images/Icon-GitHub.svg'],
+  paragraph: "In this project, you will build a basic website that allows users to add/remove books from a list. You will achieve that by using JavaScript objects and arrays. You will also need to dynamically modify the DOM and add basic events.",
+  liveversion: ['https://odaialazzeh.github.io/books/', 'See Live', 'images/Icon-Export.svg'],
+  source: ['https://github.com/odaialazzeh/books', 'See Source', 'images/Icon-GitHub.svg'],
 };
 
-const popup3 = {
-  name: 'popup3',
-  img: ['images/SnapshootPortfolio3.png', 'images/Icon-Cancel-pop.svg', 'images/Disable-pop.svg', 'images/SnapshootPortfolioD3.png'],
-  imgId: ['imgs', 'imgExit3', 'imgx3', 'imgsd'],
-  heading: 'Multi - Post stories',
-  technologies: ['Ruby on rails', 'css', 'JavScript'],
-  technologies1: ['Codekit', 'GitHub', 'Javasript', 'Bootstrap', 'Terminal', 'Codepen'],
-  paragraph: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
-  liveversion: ['#', 'See Live', 'images/Icon-Export.svg'],
-  source: ['#', 'See Source', 'images/Icon-GitHub.svg'],
-};
 
-const array = [popup, popup1, popup2, popup3];
+const array = [popup, popup1, popup2];
 
-for (let i = 0; i < 4; i += 1) {
+for (let i = 0; i < 3; i += 1) {
   const div = document.createElement('div');
   div.className = `${array[i].name}`;
   div.id = `${array[i].name}`;
+
 
   for (let y = 0; y < 4; y += 1) {
     const img = document.createElement('img');
@@ -102,7 +92,7 @@ for (let i = 0; i < 4; i += 1) {
   div.appendChild(ul1);
 
   const fragment1 = document.createDocumentFragment();
-  for (let j = 0; j < 6; j += 1) {
+  for (let j = 0; j < 3; j += 1) {
     const li1 = document.createElement('li');
     li1.textContent = `${array[i].technologies1[j]}`;
     fragment1.appendChild(li1);
@@ -141,9 +131,8 @@ for (let i = 0; i < 4; i += 1) {
 const pop = document.querySelector('.popup');
 const pop1 = document.querySelector('.popup1');
 const pop2 = document.querySelector('.popup2');
-const pop3 = document.querySelector('.popup3');
 
-const x = window.matchMedia('(max-width: 768px)');
+const x = window.matchMedia('(max-width: 738px)');
 function openPopup() {
   if (x.matches) {
     pop.classList.add('open-popup');
@@ -165,34 +154,25 @@ function openPopup2() {
   }
 }
 
-function openPopup3() {
-  if (x.matches) {
-    pop3.classList.add('open-popup');
-    document.getElementById('overlay').style.display = 'block';
-  }
-}
 
 const closeM = document.getElementById('imgExit');
 const closeM1 = document.getElementById('imgExit1');
 const closeM2 = document.getElementById('imgExit2');
-const closeM3 = document.getElementById('imgExit3');
 
 function closePopup() {
   pop.classList.remove('open-popup');
   pop1.classList.remove('open-popup');
   pop2.classList.remove('open-popup');
-  pop3.classList.remove('open-popup');
   document.body.style.backgroundColor = 'white';
   document.getElementById('overlay').style.display = 'none';
 }
 closeM.addEventListener('click', closePopup);
 closeM1.addEventListener('click', closePopup);
 closeM2.addEventListener('click', closePopup);
-closeM3.addEventListener('click', closePopup);
 
 /* popup window for Desktop-version */
 
-const x1 = window.matchMedia('(min-width: 769px)');
+const x1 = window.matchMedia('(min-width: 739px)');
 
 function openPopupD() {
   if (x1.matches) {
@@ -215,38 +195,30 @@ function openPopupD2() {
   }
 }
 
-function openPopupD3() {
-  if (x1.matches) {
-    pop3.classList.add('open-popupD');
-    document.getElementById('overlay').style.display = 'block';
-  }
-}
 
 const closeD = document.getElementById('imgx');
 const closeD1 = document.getElementById('imgx1');
 const closeD2 = document.getElementById('imgx2');
-const closeD3 = document.getElementById('imgx3');
 
 function closePopupD() {
   pop.classList.remove('open-popupD');
   pop1.classList.remove('open-popupD');
   pop2.classList.remove('open-popupD');
-  pop3.classList.remove('open-popupD');
   document.body.style.backgroundColor = 'white';
   document.getElementById('overlay').style.display = 'none';
 }
 closeD.addEventListener('click', closePopupD);
 closeD1.addEventListener('click', closePopupD);
 closeD2.addEventListener('click', closePopupD);
-closeD3.addEventListener('click', closePopupD);
 
 /* cards */
 
 const cards = {
-  heading: ['Multi-Post Stories Gain+Glory', 'Multi - Post stories Tonic', 'Profesional Art Printing Data', 'Multi - Post stories'],
-  technologies: ['Ruby on rails', 'css', 'JavScript', 'html'],
+  heading: ['Web Development Course', 'To Do List', 'Awesome books'],
+  technologies: ['CSS', 'JavScript', 'HTML'],
   link: ['#popup', '#popup1', '#popup2', '#popup3'],
-  functions: ['openPopupD(); openPopup();', 'openPopupD1(); openPopup1();', 'openPopupD2(); openPopup2();', 'openPopupD3(); openPopup3();'],
+  img: ['images/Web-d.png', 'images/list.png', 'images/books.png'],
+  functions: ['openPopupD(); openPopup();', 'openPopupD1(); openPopup1();', 'openPopupD2(); openPopup2();'],
 };
 
 const div1 = document.createElement('div');
@@ -255,32 +227,44 @@ div1.id = 'Tcontainer';
 
 const fragmentDiv = document.createDocumentFragment();
 
-for (let y = 0; y < 4; y += 1) {
+for (let y = 0; y < 3; y += 1) {
   const divCards = document.createElement('div');
   divCards.className = 'container';
+  fragmentDiv.appendChild(divCards);
+
+
   const divCards1 = document.createElement('div');
   divCards1.className = 'sub-container';
+  divCards.appendChild(divCards1);
+
+  const img = document.createElement('img');
+  img.src = `${cards.img[y]}`;
+  divCards1.appendChild(img);
+  
   const h = document.createElement('h3');
   h.textContent = `${cards.heading[y]}`;
+  divCards1.appendChild(h);
+
   const ulCrad = document.createElement('ul');
   const fragmentLi = document.createDocumentFragment();
-  for (let y = 0; y < 4; y += 1) {
+  for (let y = 0; y < 3; y += 1) {
     const liCard = document.createElement('li');
     liCard.textContent = `${cards.technologies[y]}`;
     fragmentLi.appendChild(liCard);
   }
+  divCards1.appendChild(ulCrad);
+  ulCrad.appendChild(fragmentLi);
+
   const aCard = document.createElement('a');
   aCard.href = `${cards.link[y]}`;
   aCard.textContent = 'See Project';
   aCard.id = 'seeProject';
   aCard.setAttribute('onclick', cards.functions[y]);
-
-  fragmentDiv.appendChild(divCards);
-  divCards.appendChild(divCards1);
-  divCards1.appendChild(h);
-  divCards1.appendChild(ulCrad);
-  ulCrad.appendChild(fragmentLi);
   divCards1.appendChild(aCard);
+
+  
+ 
+  
 }
 div1.appendChild(fragmentDiv);
 
